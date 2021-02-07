3b557d1828384990cbaac8d5e49a70dc06396d8d
derdesz <dora.mailbox@gmail.com>
2021-02-07 11:55:50 +0100

    add about the studies and mentoring

---
+++ src/components/survey-questions/StudiesMentoring.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const StudiesMentoring = () => {
+
+    return (
+        <React.Fragment>
+            <div className="ui hidden divider">
+            </div>
+
+            <div className="ui container white-background">
+                <div className="ui form">
+                    <div className="header">
+                        <h1>About the studies, mentoring</h1>
+                        <div className="padded">
+                            <input type="text" placeholder="Leírás (nem kötelező)"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="ui hidden divider">
+                </div>
+            </div>
+
+        </React.Fragment>
+
+    )
+        ;
+}
+
+export default StudiesMentoring;

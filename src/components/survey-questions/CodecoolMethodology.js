6b14c7fd57ad713ed797c5b91871fdd4c70e8618
derdesz <dora.mailbox@gmail.com>
2021-02-07 11:55:07 +0100

    add about codecool methodology

---
+++ src/components/survey-questions/CodecoolMethodology.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const CodecoolMethodology = () => {
+
+    return (
+        <React.Fragment>
+            <div className="ui hidden divider">
+            </div>
+
+            <div className="ui container white-background">
+                <div className="ui form">
+                    <div className="header">
+                        <h1>About Codecool methodology</h1>
+                        <ol>
+                            <li className="answer">Way less would be enough</li>
+                            <li className="answer">A little less would be enough</li>
+                            <li className="answer">It is perfect the way it is</li>
+                            <li className="answer">I need a little bit more</li>
+                            <li className="answer">I need way more</li>
+                        </ol>
+                    </div>
+                </div>
+                <div className="ui hidden divider">
+                </div>
+            </div>
+
+        </React.Fragment>
+
+    )
+        ;
+}
+
+export default CodecoolMethodology;

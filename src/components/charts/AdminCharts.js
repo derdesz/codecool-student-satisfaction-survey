11ebf57d1dad81755f1cb8d393b15310ed969c21
derdesz <dora.mailbox@gmail.com>
2021-02-08 13:38:59 +0100

    add AdminCharts component

---
+++ src/components/charts/AdminCharts.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Dropdowns from "../dropdown/Dropdowns";
+import ResponsiveBar from "./ResponsiveBar";
+
+const AdminCharts = () => {
+
+    return (
+        <div>
+            <Dropdowns/>
+            <ResponsiveBar/>
+        </div>
+    );
+}
+
+export default AdminCharts;

434737e3245c934dee3388f0982cfe890a0b57f4
derdesz <dora.mailbox@gmail.com>
2021-02-09 12:37:46 +0100

    add survey submit function

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import LoginButton from "./login/LoginButton";
 import AboutThisSurvey from "./survey-questions/AboutThisSurvey";
 import InfoAboutYou from "./survey-questions/InfoAboutYou";
@@ -22,35 +23,52 @@ import SoftSkillsHelp from "./survey-questions/SoftSkillsHelp";
 import EmotionalSupport from "./survey-questions/EmotionalSupport";
 import CodecoolMethodology from "./survey-questions/CodecoolMethodology";
 import TeamWork from "./survey-questions/Teamwork";
+import SubmitSurvey from "./SubmitSurvey";
 
 const MainPage = () => {
 
+    const surveyResult = {};
+
+
+    const onSubmit = () => {
+        // console.log(surveyResult);
+        axios({
+            method: 'POST',
+            url: '/submit-survey',
+            surveyResult: surveyResult
+        }).then(console.log(surveyResult))
+            .catch(er => {
+                console.log("no data sorry ", er);
+            });
+    }
+
+
     return (
         <React.Fragment>
-            <LoginButton/>
-            <AboutThisSurvey/>
-            <InfoAboutYou/>
-            <WhichRoom/>
-            <WhichLocation/>
-            <CalendarWeeks/>
-            <AdminAndIssues/>
-            <ResponseTimes/>
-            <StaffValidReaction/>
-            <SmoothCommunication/>
-            <CultureAtmoshereLocation/>
-            <CoolAtmosphere/>
-            <BelongToGroup/>
-            <CodecoolLocation/>
-            <CleanCalmEnvironment/>
-            <StudiesMentoring/>
-            <TheoreticalMaterials/>
-            <CurriculumRequirements/>
-            <HardSkillsHelp/>
-            <SoftSkillsHelp/>
-            <EmotionalSupport/>
-            <CodecoolMethodology/>
-            <TeamWork/>
-
+                <LoginButton/>
+                <AboutThisSurvey/>
+                <SubmitSurvey onSubmit={onSubmit}/>
+                <InfoAboutYou surveyResult={surveyResult}/>
+                <WhichRoom surveyResult={surveyResult}/>
+                <WhichLocation surveyResult={surveyResult}/>
+                <CalendarWeeks surveyResult={surveyResult}/>
+                <AdminAndIssues surveyResult={surveyResult}/>
+                <ResponseTimes surveyResult={surveyResult}/>
+                <StaffValidReaction surveyResult={surveyResult}/>
+                <SmoothCommunication surveyResult={surveyResult}/>
+                <CultureAtmoshereLocation surveyResult={surveyResult}/>
+                <CoolAtmosphere surveyResult={surveyResult}/>
+                <BelongToGroup surveyResult={surveyResult}/>
+                <CodecoolLocation surveyResult={surveyResult}/>
+                <CleanCalmEnvironment surveyResult={surveyResult}/>
+                <StudiesMentoring surveyResult={surveyResult}/>
+                <TheoreticalMaterials surveyResult={surveyResult}/>
+                <CurriculumRequirements surveyResult={surveyResult}/>
+                <HardSkillsHelp surveyResult={surveyResult}/>
+                <SoftSkillsHelp surveyResult={surveyResult}/>
+                <EmotionalSupport surveyResult={surveyResult}/>
+                <CodecoolMethodology surveyResult={surveyResult}/>
+                <TeamWork surveyResult={surveyResult}/>
         </React.Fragment>
     );
 }

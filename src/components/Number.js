f90877e39f59fad38d08e0aa9fe820bfa824507d
derdesz <dora.mailbox@gmail.com>
2021-02-09 12:37:15 +0100

    connect rating numbers to click event

--- src/components/Number.js
+++ src/components/Number.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Number = ({ratingNumbers}) => {
+const Number = ({ratingNumbers, clickCheckBox, questionId}) => {
 
     return (
         ratingNumbers.map((number) => (
-            <div className="numbers">
+            <div className="numbers" key={number}>
                 <p>{number}</p>
-                <input type="radio" name="example2"/>
+                <input type="radio" name={questionId} value={number} onClick={clickCheckBox}/>
             </div>
         ))
     );
